475e95a9675fe2f49f8ece976be25712503c2445
yusei53 <yusei530922@gmail.com>
2024-11-26 00:54:57 +0900

    setting favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "yodai birthday!!",
+  title: "yodai 22nd birthday!",
   description: "よーだい！22歳の誕生日おめでとう！！",
 };
 
@@ -15,6 +15,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="yodai 22nd" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
